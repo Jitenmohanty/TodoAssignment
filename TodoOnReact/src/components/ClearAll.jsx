ce4eb3f135e130c2fcafc8a479d7bb6b2e62201c
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-09-27 21:57:53 +0530

    Project Was Done

--- TodoOnReact/src/components/ClearAll.jsx
+++ TodoOnReact/src/components/ClearAll.jsx
@@ -1,7 +1,7 @@
-const ClearAll = () => {
+const ClearAll = ({ clearAllTodo }) => {
   return (
     <div className="float-right bg-red-500 text-white p-2 mt-4 mr-4 md:mr-96  rounded-md">
-      <button onClick={TODO}>Clear All</button>
+      <button onClick={clearAllTodo}>Clear All</button>
     </div>
   );
 };
