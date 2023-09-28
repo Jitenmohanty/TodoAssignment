333cb80f6ff6074377a727ed656ea2c667913165
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-09-28 09:36:19 +0530

    All thing Done on My side

--- TodoOnReact/src/components/ClearAll.jsx
+++ TodoOnReact/src/components/ClearAll.jsx
@@ -1,4 +1,7 @@
-const ClearAll = ({ clearAllTodo }) => {
+import { useTodo } from "../context/TodoContext";
+
+const ClearAll = () => {
+  const { clearAllTodo } = useTodo();
   return (
     <div className="float-right bg-red-500 text-white p-2 mt-4 mr-4 md:mr-96  rounded-md">
       <button onClick={clearAllTodo}>Clear All</button>
