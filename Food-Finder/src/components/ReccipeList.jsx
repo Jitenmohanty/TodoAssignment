b20ed986209043f52329d7942b89cb655e103c6d
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-10-23 20:37:26 +0530

    Recipe Details container done

--- Food-Finder/src/components/ReccipeList.jsx
+++ Food-Finder/src/components/ReccipeList.jsx
@@ -8,8 +8,8 @@ function RecipeLists(props) {
   const [query, setQuery] = useState("biriyani");
   const [data, setData] = useState("");
   const [resipeList, setRecipeList] = useState(false);
-  const [apiId , setApiId] = useState([])
-  const apiArray= [];
+  const [apiId, setApiId] = useState("");
+  const apiArray = [];
 
   const searchrecipe = (searchQuery) => {
     fetchData(searchQuery).then((response) => {
@@ -26,10 +26,20 @@ function RecipeLists(props) {
         top: document.body.scrollHeight + 3000,
         behavior: "smooth",
       });
-      console.log("Recipe List Page");
+      function extractIdFromUrl(apiUrl) {
+        const idPattern = /\/([^/]+)\?type=public/;
+        const idMatch = apiUrl.match(idPattern);
+        if (idMatch) {
+          return setApiId(idMatch[1]); // Extract the matched ID
+        } else {
+          return null; // Return null if no match is found
+        }
+      }
+      extractIdFromUrl(apiArray[index])
+      console.log(apiId)
       clearTimeout();
-      console.log(apiArray[index])
-      console.log(apiArray)
+      // console.log(apiArray[index]);
+      // console.log(apiArray);
     }, 1000);
     console.log(document.body.scrollHeight);
   };
@@ -66,11 +76,13 @@ function RecipeLists(props) {
                 <img src={item.recipe.image} alt={item.recipe.label} />
                 {apiArray.push(item._links.self.href)}
               </div>
-              <p onClick={()=>handleRecipieListPage(index)}>{item.recipe.label}</p>
+              <p onClick={() => handleRecipieListPage(index)}>
+                {item.recipe.label}
+              </p>
             </div>
           ))}
       </div>
-      {resipeList && <RecipeListItem />}
+      {resipeList && <RecipeListItem apiId={apiId} setRecipeList={setRecipeList} setApiId={setApiId} />}
     </div>
   );
 }
--- Food-Finder/src/components/RecipeListItem.jsx
+++ Food-Finder/src/components/RecipeListItem.jsx
@@ -1,33 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { GiCheckMark } from "react-icons/gi";
+import { fetchDataFromTab } from "../service/Api";
 
-const RecipeListItem = () => {
-  return (
-    <div>
-      <div className='recipe_banner'>
-                <div className="left-col">
-                    <span className='badge'>Italian</span>
-                    <h1 className='gradient-text '>Biriyani</h1>
-                    <p><strong>Recipe by:</strong><small>Bg Group</small></p>
-                    <h3 className='gradient-ingredient'>Ingredients</h3>
-                    <div className='ingredients'>
-                        <ul>
-                            <li>Alu</li>
-                            <li>Alu</li>
-                            <li>Alu</li>
-                            <li>Alu</li>
-                            <li>Alu</li>
-                            
-                        </ul>
-                    </div>
-                </div>
-                <div className="right-col">
-                    <div className="image-wrapper">
-                    <img src="https://media.istockphoto.com/id/1453499717/photo/chicken-biryani-or-biriyani-served-in-plate-isolated-on-table-top-view-indian-spicy-food.webp?b=1&s=170667a&w=0&k=20&c=UJcA-IR5iua10_r7ca41tk1c3F7fZkkjoL2_HCaEwMA=" alt="Biriyani" />
-                    </div>
-                </div>
+function RecipeListItem(props) {
+  const [tabData, setTabData] = useState(null);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    fetchDataFromTab(props.apiId)
+      .then((response) => {
+        setTabData(response);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      })
+      .finally(() => {
+        setLoader(false);
+      });
+  }, [props.apiId]);
+
+  if (loader) {
+    return (
+      <div className="container">
+        <div className="loader">
+          <div className="spinner"></div>
         </div>
+      </div>
+    );
+  }
+
+  if (!tabData || !tabData.recipe) {
+    return <h2>Container Is Empty</h2>; 
+  }
+
+  return (
+    <div className="container">
+      <h1 className="recipeHeading">What would you like to have!</h1>
+      <div className="recipe_banner">
+        <div className="left-col">
+          <span className="badge">
+            {tabData.recipe.cuisineType[0]?.toUpperCase()}
+          </span>
+          <h1 className="gradient-text">{tabData.recipe.label}</h1>
+          <p>
+            <strong>Recipe by:</strong>
+            <small>{tabData.recipe.source}</small>
+          </p>
+          <h3 className="gradient-ingredient">Ingredients</h3>
+          <div className="ingredients">
+            <ul>
+              {tabData.recipe.ingredientLines.map((list, index) => (
+                <li key={index}>
+                  <GiCheckMark size="18px" color="#6fcb9f" />
+                  &nbsp;<span>{list}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="right-col">
+          <div className="image-wrapper">
+            <img src={tabData.recipe.image} alt={tabData.recipe.label} />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default RecipeListItem
+export default RecipeListItem;
